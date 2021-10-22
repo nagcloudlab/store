bd1023ccae920ab2083ce77bd762decf330cf7f1
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2021-10-22 14:58:27 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@
 public class HelloWorld{
     public static void main(String[] args){
         //..
-        System.out.println("hello world!!")// SCM ( source code management) tool
+        System.out.println("hello world")// SCM ( source code management) tool
     }
 }
